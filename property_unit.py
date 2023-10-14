4689dd4af9a23511b5eb8de31e2492652f9aba8f
ollaidh <ollaidh@programmer.net>
2023-10-14 16:56:07 +0300

    Reorganize fields of PropertyUnit class

--- property_unit.py
+++ property_unit.py
@@ -1,14 +1,10 @@
 from dataclasses import dataclass
-from search_parameters import SearchParameters
 
 
 # stores information about single advertisement
 @dataclass
 class PropertyUnit:
-    request: SearchParameters  # by which request was this info obtained
-    id: int  # id in parsaraki database
-    date: str  # date of request
-    bedrooms: int
-    price: int
-    lat: float
-    lon: float
+    id: int  # id in bazaraki.com database
+    price_history: {str: int}  # key - date, value - price
+    bedrooms: int  # number of bedrooms
+    adv_link: str  # link to the advertisement on bazaraki.com
